fc4b985b170e0cce9205b5bf9972aa8dc3a4fe1f
pehor9164 <pehor9164@7KWN6Y1.Student.UGDSB.ED>
2018-10-19 10:21:16 -0400

    First Early Copy

---
+++ src/loopsandpowers/LoopsAndPowers.java
@@ -0,0 +1,23 @@
+/*
+ * Peter Horne-Deus
+ * LoopsAndPowers.java
+ * gets user input and outputs a number
+ * October 19,2018
+ */
+
+package loopsandpowers;
+
+/**
+ *
+ * @author pehor9164
+ */
+public class LoopsAndPowers {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
